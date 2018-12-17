8f49bc2a0f19307c7c67552b0c139141087f3cb4
Ben Bolker <bbolker@gmail.com>
2018-12-17 12:43:17 -0500

    add a bunch of stuff from glmmTMB

--- inst/vignette_data/test2.R
+++ inst/vignette_data/test2.R
@@ -4,3 +4,18 @@
 fun_cat2 = function(dummy) {
   cat(paste0("fun ", dummy))
 }
+
+## @knitr pkgs
+
+library(glmmTMB)
+library(tmbstan)
+library(coda)
+
+## @knitr fit1
+
+data("sleepstudy",package="lme4")
+fm1 <- glmmTMB(Reaction ~ Days + (Days|Subject),
+               sleepstudy)
+
+## @knitr test3
+2+2
--- vignettes/test.R
+++
@@ -1,6 +0,0 @@
-## @knitr test
-
-##' @param dummy starting value
-fun_cat = function(dummy) {
-  cat(paste0("fun ", dummy))
-}
