9251979ab7e1df17b7179ffe3db2c9f687857fc2
mollie brooks <mollieebrooks@gmail.com>
2018-12-17 17:42:16 +0100

    first try

---
+++ inst/vignette_data/test2.R
@@ -0,0 +1,6 @@
+## @knitr test2
+
+##' @param dummy starting value
+fun_cat2 = function(dummy) {
+  cat(paste0("fun ", dummy))
+}
---
+++ vignettes/test.R
@@ -0,0 +1,6 @@
+## @knitr test
+
+##' @param dummy starting value
+fun_cat = function(dummy) {
+  cat(paste0("fun ", dummy))
+}
